8a509ee9a1771b3d8c8e2dc9b67d92b6111ad347
morookaS <v21e3020@oita-u.ac.jp>
2022-06-19 14:11:01 +0000

    レッスン09の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,25 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end
+
+    def create
+        # render plain:"受信パラメーター : #{params}"
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render "new"
+        end
+    end
     
     def new
+        @post = Post.new
     end
+    
+private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+    
 end
