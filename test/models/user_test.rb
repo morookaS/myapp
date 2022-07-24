6c3ce4b395995f5a4d87fb5cdc7eaecb0b2d0793
morookaS <v21e3020@oita-u.ac.jp>
2022-07-24 15:38:07 +0000

    レッスン14の課題

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  signin_name     :string
+#  display_name    :string
+#  password_digest :string
+#  status          :string           default("available")
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
